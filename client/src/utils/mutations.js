06712a801d365f26689e6b958c839c0ae262b0a5
Chenghao Hou <houchenghao@hotmail.com>
2023-05-01 01:55:07 +1000

    updated

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,27 +44,21 @@ export const SAVE_BOOK = gql `
     }
 `;
 
-// export const REMOVE_BOOK = gql`
-//     mutation removeBook($bookId: String!){
-//         removeBook(bookId: $bookId)
-//         _id
-//         username
-//         email
-//         bookCount
-//         savedBooks{
-//             bookId
-//             authors
-//             description
-//             title
-//             image
-//             link
-//         }
-//     }
 export const REMOVE_BOOK = gql`
-    mutation removeBook($bookId: ID!) {
-        removeBook(bookId: $bookId) {
-            _id
-            username
+    mutation removeBook($bookId: String!){
+        removeBook(bookId: $bookId)
+        _id
+        username
+        email
+        bookCount
+        savedBooks{
+            bookId
+            authors
+            description
+            title
+            image
+            link
         }
     }
+
 `
